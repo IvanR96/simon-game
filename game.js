5ae381bd61fe28172c289e7c98ad9306300e605f
IvanR96 <irios96@hotmail.com>
2024-07-07 23:40:27 -0700

    playsound function made

--- game.js
+++ game.js
@@ -2,7 +2,7 @@ let gamePattern = [];
 
 let buttonColors = ["red", "blue", "green", "yellow"];
 
-
+let userClickedPattern = [];
 
 function nextSequence(){
     let randomNumber = Math.floor(Math.random() * 4);
@@ -10,8 +10,24 @@ function nextSequence(){
     let randomChosenColor = buttonColors[randomNumber];
 
     gamePattern.push(randomChosenColor);
+
+    $("#" + randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100);
+
+    playSound(randomChosenColor);
+
+    $("btn").click("click", function(){
+        let userChosenColor = $(this).attr("id");
+
+        userChosenColor.push(userClickedPattern);
+
+        playSound(userChosenColour);
+
+    });
+}
+
+function playSound(name){
+    let audio = new Audio(name + ".mp3");
+    audio.play();
 }
 
 nextSequence();
-
-$
